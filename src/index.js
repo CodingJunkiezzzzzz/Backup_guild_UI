ac4993523d4a84974582b1c44816f16a7a2d18ad
BetterLogicTeam <47892812+tayyabarine@users.noreply.github.com>
2023-08-14 23:42:54 +0500

    Final commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+
+const chains = [mainnet]
+const projectId = 'ae64d2d938316ce3350fea4c10f6cc79'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <React.StrictMode>
-      <App />
+      <WagmiConfig config={wagmiConfig}>
+        <App />
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </React.StrictMode>
   </Router>
 );
